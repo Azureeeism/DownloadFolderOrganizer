6e213bfc054282139132ae93d54deb42c1e0a238
Azureeeism <noob_gaming143@yahoo.com>
2023-08-05 03:09:44 +0800

    third commit - used chat-gpt (Update file organization logic: prioritize moving files to 'compressed' folder for known formats and fallback to 'misc' folder for unknown formats. Verify and correct 'fileFormats.py' module for accurate file formats and folder mappings.)

--- main.py
+++ main.py
@@ -10,20 +10,18 @@ files = os.listdir(DOWNLOADSFOLDER)
 for file in files:
     file_path = os.path.join(DOWNLOADSFOLDER, file)
     if os.path.isfile(file_path):
-        for file_type, extensions in file_types.items():
-            if file.endswith(extensions):
+        for file_type, file_format in file_types.items():
+            if file.endswith(file_format):
                 moveFiles.to_folder(file_path, file_type)
                 break
-            else:
-                if file.startswith("Click me to organize files"):
-                    break
-                try:
-                    destinationfolder_path = os.path.join(DOWNLOADSFOLDER + r'\misc')
-                    shutil.move(file_path, destinationfolder_path)
-                    print(f"File '{file_path}' moved to '{destinationfolder_path}'.")
-                    break
-                except Exception as e:
-                    print(f"Error moving the file: {e}")
-                    break
+        else:
+            if file.startswith("Click me to organize files"):
+                continue
+            try:
+                destinationfolder_path = os.path.join(DOWNLOADSFOLDER, 'misc')
+                shutil.move(file_path, destinationfolder_path)
+                print(f"File '{file_path}' moved to '{destinationfolder_path}'.")
+            except Exception as e:
+                print(f"Error moving the file: {e}")
     else:
         print(f"Skipping: {file}")
