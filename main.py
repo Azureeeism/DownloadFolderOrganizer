16643088144f65ee765eb0f88bd1472ac87e21cf
Azureeeism <noob_gaming143@yahoo.com>
2023-08-05 09:49:13 +0800

    updated to dynamically add the download's folder path

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import os
 from fileFormats import file_types
 import moveFiles
 import shutil
+from pathlib import Path
 
-DOWNLOADSFOLDER = r'C:\Users\LENOVO IP\Downloads'
+downloads_folder_path = str(Path.home() / "Downloads")
+downloaded_files = os.listdir(downloads_folder_path)
 
-files = os.listdir(DOWNLOADSFOLDER)
-
-for file in files:
-    file_path = os.path.join(DOWNLOADSFOLDER, file)
-    if os.path.isfile(file_path):
-        for file_type, file_format in file_types.items():
-            if file.endswith(file_format):
-                moveFiles.to_folder(file_path, file_type)
-                break
+def func():
+    for file in downloaded_files:
+        file_path = os.path.join(downloads_folder_path, file)
+        
+        if os.path.isfile(file_path):
+            for file_type, file_format in file_types.items():
+                destinationfolder_path = os.path.join(downloads_folder_path, file_type)
+                if file.endswith(file_format):
+                    moveFiles.to_folder(file_path, file_type)
+                    break
+            else:
+                if file.startswith("Clic k me to organize files"):
+                    continue
+                try:
+                    destinationfolder_path = os.path.join(downloads_folder_path, 'misc')
+                    shutil.move(file_path, destinationfolder_path)
+                    print(f"File '{file_path}' moved to '{destinationfolder_path}'.")
+                except Exception as e:
+                    print(f"Error moving the file: {e}")
         else:
-            if file.startswith("Click me to organize files"):
-                continue
-            try:
-                destinationfolder_path = os.path.join(DOWNLOADSFOLDER, 'misc')
-                shutil.move(file_path, destinationfolder_path)
-                print(f"File '{file_path}' moved to '{destinationfolder_path}'.")
-            except Exception as e:
-                print(f"Error moving the file: {e}")
-    else:
-        print(f"Skipping: {file}")
+            print(f"Skipping: {file}")
