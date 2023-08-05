01b1ff86d6d2ad779e434c96a085b7b93c4d1035
Azureeeism <noob_gaming143@yahoo.com>
2023-08-05 09:55:20 +0800

    fixed the destination folder

--- moveFiles.py
+++ moveFiles.py
@@ -1,18 +1,15 @@
 import os
 import shutil
-DOWNLOADSFOLDER = r'C:\Users\LENOVO IP\Downloads'
 
 def to_folder(source_file, destination_folder):
 
-    destinationfolder_path = os.path.join(DOWNLOADSFOLDER, destination_folder)
-
-    if not os.path.exists(destinationfolder_path):
-        os.makedirs(destinationfolder_path)
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
     try:
-        shutil.move(source_file, destinationfolder_path)
+        shutil.move(source_file, destination_folder)
 
-        print(f"File '{source_file}' moved to '{destinationfolder_path}'.")
+        print(f"File '{source_file}' moved to '{destination_folder}'.")
 
     except Exception as e:
         print(f"Error moving the file: {e}")
